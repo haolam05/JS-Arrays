2f1a53ed34c641457daac045eb7758fb5c8cbcef
Hao Lam <tuonghao2001@gmail.com>
2023-06-27 00:33:04 -0700

    transfer

--- script.js
+++ script.js
@@ -170,6 +170,16 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI();
+  }
+  inputLoanAmount.value = '';
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
