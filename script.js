d547a2478874a4a03e29d43576f08846c3f2096a
Hao Lam <tuonghao2001@gmail.com>
2023-06-26 22:27:59 -0700

    Coding Challenge 3

--- script.js
+++ script.js
@@ -252,3 +252,12 @@ const calcAverageHumanAge = function (ages) {
 };
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+// Coding Challenge #3
+const calcAverageHumanAge2 = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length);
+console.log(calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]));
