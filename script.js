55752b6b540da351eaef0f0d6a19322aa14b1903
Hao Lam <tuonghao2001@gmail.com>
2023-06-26 18:53:57 -0700

    Coding Challenge #1

--- script.js
+++ script.js
@@ -169,4 +169,16 @@ currenciesUnique.forEach(function (value, _, set) {
 });
 
 // Coding Challenge #1
- 
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+  const allDogs = dogsJuliaCorrected.concat(dogsKate);
+  allDogs.forEach(function (age, i) {
+    if (age < 3) {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    } else {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    }
+  });
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
